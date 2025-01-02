83b85b63b451366076ee58375121b270ee6fdc60
Nishikant Singh <nishikantsingh2004@gmail.com>
2025-01-02 13:11:13 +0530

    moved user page style to public styles file and updated dummy employee data

--- schema.sql
+++ schema.sql
@@ -7,23 +7,38 @@ USE hrms;
 CREATE TABLE employee (
     Id int NOT NULL AUTO_INCREMENT, 
     Name VARCHAR(50) NOT NULL, 
+
+    DateJoined DATE NOT NULL,
+
+    Department ENUM("IT", "RESEARCH"),
+    SubDepartment VARCHAR(50),
+    Designation ENUM("HR", "SALES"),
+
+    PaymentMode Enum("BANK TRANSFER", "Cash"),
+    Bank VARCHAR(50),
+    BankIFSC VARCHAR(20),
+    BankAccount VARCHAR(30),
+    UAN VARCHAR(30),
+    PFNumber VARCHAR(30),
+    PANNumber VARCHAR(20),
+
     PRIMARY KEY (Id)
 );
 
 -- temporarily create dummy users
 INSERT INTO employee
-    (Name)
+    (Name, DateJoined, Department, SubDepartment, Designation, PaymentMode, Bank, BankIFSC, BankAccount, UAN, PFNumber, PANNumber)
 VALUES
-    ("John"),
-    ("Alice"),
-    ("Bob"),
-    ("Tom"),
-    ("Alex"),
-    ("Casey"),
-    ("Taylor"),
-    ("Morgan"),
-    ("Dakota"),
-    ("Jordan");
+    ("John", "2024-01-01", "IT", "NA", "HR", "BANK TRANSFER", "SBI", "SBI000055558888", "4000555666777", "UAN12345", "PF123356", "PAN12345"),
+    ("Alice", "2024-01-01", "IT", "NA", "HR", "BANK TRANSFER", "SBI", "SBI000055558888", "4000555666777", "UAN12345", "PF123356", "PAN12345"),
+    ("Bob", "2024-01-01", "IT", "NA", "HR", "BANK TRANSFER", "SBI", "SBI000055558888", "4000555666777", "UAN12345", "PF123356", "PAN12345"),
+    ("Tom", "2024-01-01", "IT", "NA", "HR", "BANK TRANSFER", "SBI", "SBI000055558888", "4000555666777", "UAN12345", "PF123356", "PAN12345"),
+    ("Alex", "2024-01-01", "IT", "NA", "HR", "BANK TRANSFER", "SBI", "SBI000055558888", "4000555666777", "UAN12345", "PF123356", "PAN12345"),
+    ("Casey", "2024-01-01", "IT", "NA", "HR", "BANK TRANSFER", "SBI", "SBI000055558888", "4000555666777", "UAN12345", "PF123356", "PAN12345"),
+    ("Taylor", "2024-01-01", "IT", "NA", "HR", "BANK TRANSFER", "SBI", "SBI000055558888", "4000555666777", "UAN12345", "PF123356", "PAN12345"),
+    ("Morgan", "2024-01-01", "IT", "NA", "HR", "BANK TRANSFER", "SBI", "SBI000055558888", "4000555666777", "UAN12345", "PF123356", "PAN12345"),
+    ("Dakota", "2024-01-01", "IT", "NA", "HR", "BANK TRANSFER", "SBI", "SBI000055558888", "4000555666777", "UAN12345", "PF123356", "PAN12345"),
+    ("Jordan", "2024-01-01", "IT", "NA", "HR", "BANK TRANSFER", "SBI", "SBI000055558888", "4000555666777", "UAN12345", "PF123356", "PAN12345");
 
 CREATE TABLE attendance(
     Id int NOT NULL AUTO_INCREMENT,
